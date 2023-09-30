b1349bf0531efeed5811b94dc38f13cbd96e023b
AmelChenni <chenniamel45@gmail.com>
2023-09-29 20:56:44 -0500

    upload engines

--- Src/Modules/app.router.js
+++ Src/Modules/app.router.js
@@ -4,21 +4,18 @@ import userRouter from "./Users/User.router.js";
 import connectDB from "../../DB/Connection.js";
 import cors from 'cors';
 
-function ignoreFavicon(req, res, next) {
-    if (req.originalUrl.includes('favicon.ico')) {
-      res.status(204).end()
-    }
-    next();
-  }
+
 
 const initApp=(app,express) => {
     connectDB();
     app.use(cors())
-    app.use(ignoreFavicon);
     app.use(express.json());
     app.use('/messages',messageRouter)
     app.use('/auth',authRouter)
     app.use('/user',userRouter)
+    app.get('/',(req, res) => {
+        return res.json({message:"welcome mesage"})
+    })
     app.use('/*',(req,res)=>{
         return res.json({message :"page not found"})
     })
